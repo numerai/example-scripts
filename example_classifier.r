50fdb731f8cedeafb1a38eda3384f45598839d65
Xander Dunn <xander.dunn@icloud.com>
2017-01-06 19:37:10 -0800

    Improvements to Krishna's script

--- example_classifier.r
+++ example_classifier.r
@@ -1,17 +1,45 @@
-# load randomForest Library
+#!/usr/bin/env Rscript
+
+# Example classifier on Numerai data using random forest classifier.
+# To get started, install the required packages: install.packages(randomForest, cvTools)
+
 require(randomForest)
-# load the training data
+
+MultiLogLoss <- function(act, pred){
+  eps <- 1e-15
+  pred <- pmin(pmax(pred, eps), 1 - eps)
+  sum(act * log(pred) + (1 - act) * log(1 - pred)) * -1/NROW(act)
+}
+
+# Set seed for reproducibility
+set.seed(0)
+
+print("Loading data...")
 train <- read.csv("numerai_training_data.csv", head=T)
-#set the seed for reproducibility
-set.seed(98415)
-# fit a randomForest model
-fit <- randomForest(as.factor(target) ~ ., data=train, importance=TRUE, do.trace = TRUE, ntree=50)
-#plot the variable importance
-varImpPlot(fit)
-#load the test data
 test <- read.csv("numerai_tournament_data.csv", head=T)
-Prediction <- predict(fit, test[,-1],type="prob")
+
+print("Testing...")
+folds <- cut(seq(1, nrow(train)), breaks=3, labels=FALSE)
+total <- 0.0
+for(i in 1:3) {
+    testIndexes <- which(folds==i, arr.ind=TRUE)
+    testData <- train[testIndexes, ]
+    trainData <- train[-testIndexes, ]
+    fit <- randomForest(as.factor(target) ~., data=trainData, importance=FALSE, do.trace=TRUE, ntree=50)
+    prediction <- predict(fit, testData, type="prob")
+    loss <- MultiLogLoss(testData["target"], prediction)
+    print(loss)
+    total <- total + loss
+}
+sprintf("Mean cross validation log loss: %f", total/3)
+
+print("Training...")
+fit <- randomForest(as.factor(target) ~ ., data=train, importance=FALSE, do.trace=TRUE, ntree=50)
+
+print("Predicting...")
+Prediction <- predict(fit, test[,-1], type="prob")
 test$probability <- Prediction[,"1"]
-pred <- test[,c("t_id","probability")]
-#write the prediction file for submission
-write.csv(pred, file="example_pred.csv", quote=F, row.names=F)
+pred <- test[,c("t_id", "probability")]
+
+print("Writing predictions to submission.csv")
+write.csv(pred, file="submission.csv", quote=F, row.names=F)
