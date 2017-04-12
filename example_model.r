9630d519f882157b8216f5047f8e2bc691b412e5
Krishna Kesavan <krishna.kesavan@gmail.com>
2017-04-11 17:14:00 -0700

    Added validation rows to test and changed back file names to include numerai

--- example_model.r
+++ example_model.r
@@ -7,13 +7,13 @@ set.seed(0)
 
 print("Loading data...")
 # Load the data from the CSV files
-train <- read.csv("training_data.csv", head=T)
-test <- read.csv("tournament_data.csv", head=T)
+train <- read.csv("numerai_training_data.csv", head=T)
+test <- read.csv("numerai_tournament_data.csv", head=T)
 
 print("Removing all columns from train other than features and target...")
 train <- train[,grep("feature|target",names(train))]
-print("Selecting test and live rows and Removing all columns from test other than features and id...")
-test  <- test[test$data_type %in% c("test","live"),grep("id|feature",names(test))]
+print("Removing all columns from test other than features and id...")
+test  <- test[,grep("id|feature",names(test))]
 
 print("Training...")
 # This is your model that will learn to predict. Your model is trained on the numerai_training_data
