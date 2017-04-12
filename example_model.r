3ec55ef2a4303b6f6c8a4aa9820d80a51248f78f
Krishna Kesavan <krishna.kesavan@gmail.com>
2017-04-11 17:09:14 -0700

    Updated the example R script.

--- example_model.r
+++ example_model.r
@@ -7,8 +7,13 @@ set.seed(0)
 
 print("Loading data...")
 # Load the data from the CSV files
-train <- read.csv("numerai_training_data.csv", head=T)
-test <- read.csv("numerai_tournament_data.csv", head=T)
+train <- read.csv("training_data.csv", head=T)
+test <- read.csv("tournament_data.csv", head=T)
+
+print("Removing all columns from train other than features and target...")
+train <- train[,grep("feature|target",names(train))]
+print("Selecting test and live rows and Removing all columns from test other than features and id...")
+test  <- test[test$data_type %in% c("test","live"),grep("id|feature",names(test))]
 
 print("Training...")
 # This is your model that will learn to predict. Your model is trained on the numerai_training_data
@@ -18,7 +23,7 @@ print("Predicting...")
 # Your trained model is now used to make predictions on the numerai_tournament_data
 predictions <- predict(model, test[,-1], type="response")
 test$probability <- predictions
-pred <- test[,c("t_id", "probability")]
+pred <- test[,c("id", "prediction")]
 
 print("Writing predictions to predictions.csv")
 # Save the predictions out to a CSV file
