9d8838ef6be0a759bfb3b6ff043bbf9f4e288f45
Keith Goodman <kwgoodman@gmail.com>
2018-09-24 11:57:28 -0700

    update numerox example for numerox 3.1.0

--- numerox_example.py
+++ numerox_example.py
@@ -10,11 +10,8 @@ def numerox_example():
     For more information see: https://github.com/kwgoodman/numerox
     """
 
-    # download dataset from numerai
-    nx.download('numerai_dataset.zip', verbose=True)
-
-    # load numerai dataset
-    data = nx.load_zip('numerai_dataset.zip', verbose=True)
+    # download dataset from numerai, save it and then load it
+    data = nx.download('numerai_dataset.zip')
 
     # we will use logistic regression; you will want to write your own model
     model = nx.logistic()
@@ -26,8 +23,8 @@ def numerox_example():
     prediction.to_csv('logistic.csv', verbose=True)
 
     # upload predictions to Numerai to enter the tournament
+    # create the public_id and secret_key on the Numerai website
     #
-    # you create the public_id and secret_key on the Numerai website
     # nx.upload('logistic.csv', tournament='bernie', public_id, secret_key)
 
 
