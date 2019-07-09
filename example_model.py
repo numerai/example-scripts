e35c6b8a68f0700990b81953fcf73df7640e77a9
Ralph Furman <ralph@numer.ai>
2019-07-09 14:29:54 -0700

    Update example for kazutsugi

--- example_model.py
+++ example_model.py
@@ -1,136 +1,65 @@
 #!/usr/bin/env python
 """
-Example classifier on Numerai data using a logistic regression classifier.
-To get started, install the required packages: pip install pandas, numpy, sklearn
+Example classifier on Numerai data using a xgboost regression.
+To get started, install the required packages: pip install pandas numpy sklearn xgboost
 """
 
 import pandas as pd
 import numpy as np
-from sklearn import decomposition, ensemble, metrics, pipeline
-from xgboost import XGBClassifier
+from xgboost import XGBRegressor
+
+
+TOURNAMENT_NAME = "kazutsugi"
+TARGET_NAME = f"target_{TOURNAMENT_NAME}"
+PREDICTION_NAME = f"predict_{TOURNAMENT_NAME}"
+
+BENCHMARK = 0.002
+BAND = 0.04
+
+
+# Submissions are scored by spearman correlation
+def score(df):
+    return df[[TARGET_NAME, PREDICTION_NAME]].corr(method="spearman")[TARGET_NAME][PREDICTION_NAME]
+
+
+# The payout function
+def payout(scores):
+    return ((scores - BENCHMARK)/BAND).clip(lower=-1, upper=1)
 
 
 def main():
-    # Set seed for reproducibility
-    np.random.seed(0)
 
     print("# Loading data...")
     # The training data is used to train your model how to predict the targets.
-    train = pd.read_csv('numerai_training_data.csv', header=0)
+    training_data = pd.read_csv("numerai_training_data.csv").set_index("id")
     # The tournament data is the data that Numerai uses to evaluate your model.
-    tournament = pd.read_csv('numerai_tournament_data.csv', header=0)
+    tournament_data = pd.read_csv("numerai_tournament_data.csv").set_index("id")
 
-    # The tournament data contains validation data, test data and live data.
-    # Validation is used to test your model locally so we separate that.
-    validation = tournament[tournament['data_type'] == 'validation']
+    feature_names = [f for f in training_data.columns if f.startswith("feature")]
+    print(f"Loaded {len(feature_names)} features")
 
-    # There are multiple targets in the training data which you can choose to model using the features.
-    # Numerai does not say what the features mean but that's fine; we can still build a model.
-    # Here we select the bernie_target.
-    train_bernie = train.drop([
-        'id', 'era', 'data_type', 'target_charles', 'target_elizabeth',
-        'target_jordan', 'target_ken', 'target_frank', 'target_hillary'
-    ],
-                              axis=1)
+    print("Training model")
+    model = XGBRegressor(max_depth=5, learning_rate=0.01, n_estimators=2000, n_jobs=-1, colsample_bytree=0.1)
+    model.fit(training_data[feature_names], training_data[TARGET_NAME])
 
-    # Transform the loaded CSV data into numpy arrays
-    features = [f for f in list(train_bernie) if "feature" in f]
-    X = train_bernie[features]
-    Y = train_bernie['target_bernie']
-    x_prediction = validation[features]
-    ids = tournament['id']
+    print("Generating predictions")
+    training_data[PREDICTION_NAME] = model.predict(training_data[feature_names])
+    tournament_data[PREDICTION_NAME] = model.predict(tournament_data[feature_names])
 
-    # This is your model that will learn to predict this target.
-    model = pipeline.make_pipeline(
-	decomposition.PCA(),
-	ensemble.BaggingClassifier(
-	    XGBClassifier(),
-	    n_jobs=-1,
-	    max_samples=0.5
-	)
-    )
-    print("# Training...")
-    # Your model is trained on train_bernie
-    model.fit(X, Y)
+    # Check the per-era correlations on the training set
+    train_correlations = training_data.groupby("era").apply(score)
+    print(f"On training the correlation has mean {train_correlations.mean()} and std {train_correlations.std()}")
+    print(f"On training the average per-era payout is {payout(train_correlations).mean()}")
 
-    print("# Predicting...")
-    # Based on the model we can predict the probability of each row being
-    # a bernie_target in the validation data.
-    # The model returns two columns: [probability of 0, probability of 1]
-    # We are just interested in the probability that the target is 1.
-    y_prediction = model.predict_proba(x_prediction)
-    probabilities = y_prediction[:, 1]
-    print("- probabilities:", probabilities[1:6])
+    # Check the per-era correlations on the validation set
+    validation_data = tournament_data[tournament_data.data_type == "validation"]
+    validation_correlations = validation_data.groupby("era").apply(score)
+    print(f"On validation the correlation has mean {validation_correlations.mean()} and std {validation_correlations.std()}")
+    print(f"On validation the average per-era payout is {payout(validation_correlations).mean()}")
 
-    # We can see the probability does seem to be good at predicting the
-    # true target correctly.
-    print("- target:", validation['target_bernie'][1:6])
-    print("- rounded probability:", [round(p) for p in probabilities][1:6])
-
-    # But overall the accuracy is very low.
-    correct = [
-        round(x) == y
-        for (x, y) in zip(probabilities, validation['target_bernie'])
-    ]
-    print("- accuracy: ", sum(correct) / float(validation.shape[0]))
-
-    # The targets for each of the tournaments are very correlated.
-    tournament_corr = np.corrcoef(validation['target_bernie'],
-                                  validation['target_elizabeth'])
-    print("- bernie vs elizabeth corr:", tournament_corr)
-    # You can see that target_elizabeth is accurate using the bernie model as well.
-    correct = [
-        round(x) == y
-        for (x, y) in zip(probabilities, validation['target_elizabeth'])
-    ]
-    print("- elizabeth using bernie:",
-          sum(correct) / float(validation.shape[0]))
-
-    # Numerai measures models on AUC. The higher the AUC the better.
-    # Numerai only pays models with AUC that beat the benchmark on the live portion of the tournament data.
-    # Our validation AUC isn't very good.
-    print("- validation AUC:",
-          metrics.roc_auc_score(validation['target_bernie'], probabilities))
-
-    # To submit predictions from your model to Numerai, predict on the entire tournament data.
-    x_prediction = tournament[features]
-    y_prediction = model.predict_proba(x_prediction)
-    results = y_prediction[:, 1]
-
-    print("# Creating submission...")
-    # Create your submission
-    results_df = pd.DataFrame(data={'probability': results})
-    joined = pd.DataFrame(ids).join(results_df)
-    print("- joined:", joined.head())
-
-    print("# Writing predictions to bernie_submissions.csv...")
-    # Save the predictions out to a CSV file.
-    joined.to_csv("bernie_submission.csv", index=False)
+    tournament_data[PREDICTION_NAME].to_csv(TOURNAMENT_NAME + "_submission.csv")
     # Now you can upload these predictions on https://numer.ai
 
-
-"""
-TIPS TO IMPROVE YOUR MODEL
-
-1. Use eras
-In this example, we dropped era column but you can use the era column to improve peformance across eras
-You can take a model like the above and use it to generate probabilities on the training data, and
-look at the the eras where your model was <0.693 and then build a new model on those bad eras to
-combine with your main model. In this way, you may be hedged to the risk of bad eras in the future.
-Advanced tip: To take this further, you could add the objective of doing consistenty well across all eras
-directly into the objective function of your machine learning model.
-
-2. Use feature importance
-As per above, you don't want your model to rely too much on any particular type of era. Similarly, you
-don't want your model to rely too much on any particular type of feature. If your model relies heavily on
-one feature (in linear regression, some feature has very high coefficient), then if that feature doesn't work
-in a particular era then your model will perform poorly. If your model balances its use of features then it is
-more likely to be consistent across eras.
-
-3. Use all the targets
-As we saw above, a model trained on one target like target_bernie might be good at predicting another target
-like target_elizabeth. Blending models built on each target could also improve your AUC.
-"""
 
 if __name__ == '__main__':
     main()
--- example_model.r
+++ example_model.r
@@ -1,75 +1,164 @@
 #Tutorial and Tips for Numerai
+
+#***
+#The stock market is the hardest data science problem in the world.
+#If it seems easy, you're doing it wrong.
+#We don't know how to solve this problem. But here is what we know.
+#We need your help. You take it from here.
+#***
 library(Metrics)
 library(gbm)
-setwd("~/Downloads/numerai_datasets/")
+library(randomForest)
+
+print("THIS SCRIPT DOWNSAMPLES DATA AND WILL LIKELY UNDERPERFORM THE example_predictions.csv")
+
 #the training data is used to train your model how to predict the targets
-train<-read.csv("numerai_training_data.csv", header=T)
+training_data<-read.csv("numerai_training_data.csv", header=T)
 #the tournament data is the data that Numerai uses to evaluate your model
 tournament<-read.csv("numerai_tournament_data.csv", header=T)
 #the tournament data contains validation data, test data and live data
-#validation is used to test your model locally so we separate that
+#validation is a hold out set out of sample and further in time (higher eras) than the training data
 validation<-tournament[tournament$data_type=="validation",]
 
-#There are multiple targets in the training data which you can choose to model using the features.
-#Numerai does not say what the features mean but that's fine; we can still build a model.
-#Here we select the bernie_target
-train_bernie<-subset(train, select=-c(id, era, data_type, target_charles, target_elizabeth, target_jordan, target_ken, target_frank, target_hillary))
-head(train_bernie)
+consistency<-function(data_with_probs, target_name){
+  unique_eras<-unique(data_with_probs$era)
+  era_corscore<-vector()
+  i<-1
+  while(i<=length(unique_eras)){
+    this_data<-data_with_probs[data_with_probs$era==unique_eras[i],]
+    era_corscore[i]<-cor(this_data[,names(this_data)==target_name], this_data$prediction, method="spearman")
+    i<-i+1
+  }
+  consistency<-sum(era_corscore>0)/length(era_corscore)
+  consistency
+}
 
-#build a gbm model to predict this target
-model<-gbm(target_bernie~., data=train_bernie, n.trees=10, shrinkage=0.01, interaction.depth=2, train.fraction=1, bag.fraction=0.5, verbose=T)
-#look at the relative importance of the features (you can see the model relies strongly on certain features)
+#the training data is large so we reduce it by 10x in order to speed up this script
+#NB reducing the training data like this is not recommended for a production ready model
+nrow(training_data)
+set.seed(10)
+#remove the following line to not downsample. Note: it will take much longer to run
+sample_rows<-sample(1:nrow(training_data), ceiling(nrow(training_data)/10))
+train<-training_data[sample_rows,]
+
+#we remove the id, era, and data_type columns for now
+train<-train[,-c(1:3)]
+nrow(train)
+
+#there are a large number of features
+ncol(train)
+#features are a number of feature groups of different sizes (intelligence, charisma, strength, dexterity, constitution, wisdom)
+#Numerai does not disclose what the features or feature groups mean; they are abstract and obfuscated
+#however, features within feature groups tend to be related in some way
+#models which have large feature importance in one group tend to do badly for long stretches of time (eras).
+head(names(train),20)
+
+#the target variable has 5 ordinal outcomes (0,0.25,0.5,0.75,1)
+#we train a model on the features to predict this target
+summary(train$target_kazutsugi)
+
+#on this dataset, feature importance analysis is very important
+#we build a random forest to understand which features tend to improve the model out of bag
+#because stocks within eras are not independent, we use small bag sizes (sampsize=10%) so the out of bag estimate is meaningful
+#we use a small subset of features for each tree to build a more feature balanced forest (mtry=10%)
+set.seed(10)
+forest<-randomForest(target_kazutsugi~., data=train, ntree=50, mtry=ceiling(0.1*ncol(train)-1), sampsize=ceiling(0.1*nrow(train)), importance=T, maxnodes=5)
+#a good model might drop the bad features according to the forest before training a final model
+#if a feature group or feature is too good, it might also be a good idea to drop to improve the feature balance and improve consistency of the model
+imp<-importance(forest)
+imp<-imp[order(imp[,1], decreasing = FALSE),]
+head(imp)
+
+#based on the forest we can generate predictions on the validation set
+#the validation set contains eras further in time than the training set
+#the validation set is not "representative of the live data"; no one knows how the live data will behave
+#usually train performance < validation performance < live performance because live data is many eras into the future from the training data
+predictions<-predict(forest, validation)
+head(predictions)
+#Numerai measures performance based on Rank Correlation between your predictions and the true targets
+cor(validation$target_kazutsugi, predictions, method="spearman")
+val<-validation
+val$prediction<-predictions
+#consistency is the fraction of months where the model achieves better than zero correlation with the targets (>0.5 CorScore)
+consistency(val, "target_kazutsugi")
+
+#we try a gbm model; we also choose a low bag fraction of 10% as a strategy to deal with within-era non-independence (which is a property of this data)
+#(if you take a sample from one era and a different sample from the same era that sample is not really out of sample because the observations occured in the same era)
+#having small bags also improves the out of bag estimate for the optimal number of trees
+set.seed(10)
+model<-gbm(target_kazutsugi~., data=train, n.trees=50, shrinkage=0.01, interaction.depth=5, train.fraction=1, bag.fraction=0.1, verbose=T)
+#looking at the relative importance of the features we can see the model relies more on some features than others
 head(summary(model))
+best.iter <- gbm.perf(model, method="OOB")
+best.iter
 
-#based on the model we can predict the probability of each row being a bernie_target in the validation data
-probabilities<-predict.gbm(model, validation, n.trees=10, type="response")
-head(probabilities)
+#we check performance of the gbm model on the out of sample validation data
+predictions<-predict.gbm(model, validation, n.trees=best.iter, type="response")
+head(predictions)
+cor(validation$target_kazutsugi, predictions, method="spearman")
+val<-validation
+val$prediction<-predictions
+consistency(val, "target_kazutsugi")
 
-#we can see the probability does seem to be good at predicting the true target correctly
-validation$target_bernie[1:6]
-round(probabilities[1:6])
+#the gbm model and random forest model have the same consistency (number of eras where CorScore is greater than 0.5) even though corScores are different
+#improving consistency can be more important than improving standard machine learning metrics like RMSE
+#good models might train in such a way as to minimize the error across eras (improve consistency) not just reduce the error on each training example
 
-#but overall the accuracy is very low
-sum(round(probabilities)==validation$target_bernie)/nrow(validation)
+#so far we have ignored eras for training
+#eras are in order of time; for example, era4 is before era5
+#the reason to use eras is that cross validation on rows will tend to overfit (rows within eras are not independent)
+#so it's much better to cross validate within eras for example: take a subset of eras, build a model and test on the out of sample subset of eras
+#this will give a better estimate of how well your model will generalize
+#the validation set is not special; it is just an out of sample set of eras greater than the training set
+#some users might choose to train on the validation set as well
 
-#the targets for each of the tournaments are very correlated
-cor(validation$target_bernie, validation$target_elizabeth)
-#you can see that target_elizabeth is accurate using the bernie model as well
-sum(round(probabilities)==validation$target_elizabeth)/nrow(validation)
+#to give you a sense of how to use eras we train a model on the first half of the eras and test it on the second half
+#we take another 10x smaller sample of data to speed up the script
+ordered_eras<-unique(training_data$era)
+train<-training_data[sample_rows,]
+first_half<-train[train$era%in%ordered_eras[1:ceiling(length(ordered_eras)/2)],]
+second_half<-train[!(train$id%in%first_half$id),]
 
-#Numerai measures models on AUC. The higher the AUC the better.
-#Numerai only pays models with AUC that beat the benchmark on the live portion of the tournament data.
-#Our validation AUC isn't very good.
-auc(validation$target_bernie, probabilities)
+#we remove id, era, data_type column and train a gbm model on the first half of the data
+set.seed(10)
+model<-gbm(target_kazutsugi~., data=first_half[,-c(1:3)], n.trees=100, shrinkage=0.01, interaction.depth=5, train.fraction=1, bag.fraction=0.1, verbose=T)
+best.iter <- gbm.perf(model, method="OOB")
+best.iter
 
+predictions<-predict.gbm(model, second_half, n.trees=best.iter, type="response")
+#our correlation score is good; what we appeared to learn generalizes well on the second half of the eras
+0.5+cor(second_half$target_kazutsugi, predictions, method="spearman")
+sec<-second_half
+sec$prediction<-predictions
+#consistency is the fraction of months where the model achieves better than zero correlation with the targets (>0.5 CorScore)
+consistency(sec, "target_kazutsugi")
+
+#but now we try build a model on the second half of the eras and predict on the first half
+set.seed(10)
+model<-gbm(target_kazutsugi~., data=second_half[,-c(1:3)], n.trees=100, shrinkage=0.01, interaction.depth=5, train.fraction=1, bag.fraction=0.1, verbose=T)
+best.iter <- gbm.perf(model, method="OOB")
+best.iter
+
+predictions<-predict.gbm(model, first_half, n.trees=best.iter, type="response")
+#our correlation score is surprisingly low and surprisingly different from when we trained on the first half
+#this means our model is not very consistent, and it's possible that we will see unexpectedly low performance on the test set or live set
+#it also shows that our validation performance is likely to greatly overestimate our performance--this era-wise cross validation is more valuable
+#a model whose performance when training on the first half is the same as training on the second half would likely be more consistent
+#and would likely perform better on the tournament data and the live data
+cor(first_half$target_kazutsugi, predictions, method="spearman")
+fir<-first_half
+fir$prediction<-predictions
+#consistency is the fraction of months where the model achieves better than zero correlation with the targets (>0.5 CorScore)
+consistency(fir, "target_kazutsugi")
+
+#Numerai only pays models with correlations that beat the benchmark on the live portion of the tournament data
 #to submit predictions from your model to Numerai, predict on the entire tournament data
-tournament$probability_bernie<-predict.gbm(model, tournament, n.trees=10, type="response")
+#we choose use our original forest model for our final submission
+tournament$prediction_kazutsugi<-predict(forest, tournament)
 
 #create your submission
-submission<-subset(tournament, select=c(id, probability_bernie))
+submission<-subset(tournament, select=c(id, prediction_kazutsugi))
 head(submission)
 
-#save your submission and now upload it to Numerai on https://numer.ai
-write.csv(submission, file="bernie_submission2.csv", row.names=F)
-
-
-#TIPS TO IMPROVE YOUR MODEL
-
-#1. Use eras
-#In this example, we dropped era column but you can use the era column to improve peformance across eras
-#You can take a model like the above and use it to generate probabilities on the training data, and
-#look at the the eras where your model was <0.693 and then build a new model on those bad eras to
-#combine with your main model. In this way, you may be hedged to the risk of bad eras in the future.
-#Advanced tip: To take this further, you could add the objective of doing consistenty well across all eras
-#directly into the objective function of your machine learning model.
-
-#2. Use feature importance
-#As per above, you don't want your model to rely too much on any particular type of era. Similarly, you
-#don't want your model to rely too much on any particular type of feature. If your model relies heavily on
-#one feature (in linear regression, some feature has very high coefficient), then if that feature doesn't work
-#in a particular era then your model will perform poorly. If your model balances its use of features then it is
-#more likely to be consistent across eras.
-
-#3. Use all the targets
-#As we saw above, a model trained on one target like target_bernie might be good at predicting another target
-#like target_elizabeth. Blending models built on each target could also improve your AUC.
+#save your submission and now upload it to https://numer.ai
+write.csv(submission, file="kazutsugi_submission.csv", row.names=F)
