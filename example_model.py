2292e1464db305a28fbb00e1dc2943d412e1031d
Michael Phillips <michael@numer.ai>
2020-09-04 21:22:56 -0700

    add code for validation diagnostics

--- example_model.py
+++ example_model.py
@@ -19,11 +19,14 @@ MODEL_FILE = Path("example_model.xgb")
 
 
 # Submissions are scored by spearman correlation
+def correlation(predictions, targets):
+    ranked_preds = predictions.rank(pct=True, method="first")
+    return np.corrcoef(ranked_preds, targets)[0, 1]
+
+
+# convenience method for scoring
 def score(df):
-    # method="first" breaks ties based on order in array
-    pct_ranks = df[PREDICTION_NAME].rank(pct=True, method="first")
-    targets = df[TARGET_NAME]
-    return np.corrcoef(targets, pct_ranks)[0, 1]
+    return correlation(df[PREDICTION_NAME], df[TARGET_NAME])
 
 
 # Payout is just the score cliped at +/-25%
@@ -31,7 +34,7 @@ def payout(scores):
     return scores.clip(lower=-0.25, upper=0.25)
 
 
-# Read the csv file into a pandas Dataframe
+# Read the csv file into a pandas Dataframe as float16 to save space
 def read_csv(file_path):
     with open(file_path, 'r') as f:
         column_names = next(csv.reader(f))
@@ -83,6 +86,7 @@ def main():
     print(f"On training the correlation has mean {train_correlations.mean()} and std {train_correlations.std()}")
     print(f"On training the average per-era payout is {payout(train_correlations).mean()}")
 
+    """Validation Metrics"""
     # Check the per-era correlations on the validation set (out of sample)
     validation_data = tournament_data[tournament_data.data_type == "validation"]
     validation_correlations = validation_data.groupby("era").apply(score)
@@ -90,8 +94,115 @@ def main():
           f"std {validation_correlations.std()}")
     print(f"On validation the average per-era payout is {payout(validation_correlations).mean()}")
 
+    # Check the "sharpe" ratio on the validation set
+    validation_sharpe = validation_correlations.mean() / validation_correlations.std()
+    print(f"Validation Sharpe: {validation_sharpe}")
+
+    print("checking max drawdown...")
+    rolling_max = (validation_correlations + 1).cumprod().rolling(window=100,
+                                                                  min_periods=1).max()
+    daily_value = (validation_correlations + 1).cumprod()
+    max_drawdown = -(rolling_max - daily_value).max()
+    print(f"max drawdown: {max_drawdown}")
+
+    # Check the feature exposure of your validation predictions
+    feature_exposures = validation_data[feature_names].apply(lambda d: correlation(validation_data[PREDICTION_NAME], d),
+                                                             axis=0)
+    max_feature_exposure = np.max(np.abs(feature_exposures))
+    print(f"Max Feature Exposure: {max_feature_exposure}")
+
+    # Check feature neutral mean
+    print("Calculating feature neutral mean...")
+    feature_neutral_mean = get_feature_neutral_mean(validation_data)
+    print(f"Feature Neutral Mean is {feature_neutral_mean}")
+
+    # Load example preds to get MMC metrics
+    example_preds = pd.read_csv("example_predictions_target_kazutsugi.csv").set_index("id")["prediction_kazutsugi"]
+    validation_example_preds = example_preds.loc[validation_data.index]
+    validation_data["ExamplePreds"] = validation_example_preds
+
+    print("calculating MMC stats...")
+    # MMC over validation
+    mmc_scores = []
+    corr_scores = []
+    for _, x in validation_data.groupby("era"):
+        series = neutralize_series(pd.Series(unif(x[PREDICTION_NAME])),
+                                   pd.Series(unif(x["ExamplePreds"])))
+        mmc_scores.append(np.cov(series, x[TARGET_NAME])[0, 1] / (0.29 ** 2))
+        corr_scores.append(correlation(unif(x[PREDICTION_NAME]), x[TARGET_NAME]))
+
+    val_mmc_mean = np.mean(mmc_scores)
+    val_mmc_std = np.std(mmc_scores)
+    val_mmc_sharpe = val_mmc_mean / val_mmc_std
+    corr_plus_mmcs = [c + m for c, m in zip(corr_scores, mmc_scores)]
+    corr_plus_mmc_sharpe = np.mean(corr_plus_mmcs) / np.std(corr_plus_mmcs)
+    corr_plus_mmc_mean = np.mean(corr_plus_mmcs)
+    corr_plus_mmc_sharpe_diff = corr_plus_mmc_sharpe - validation_sharpe
+
+    print(
+        f"MMC Mean: {val_mmc_mean}\n"
+        f"Corr Plus MMC Sharpe:{corr_plus_mmc_sharpe}\n"
+        f"Corr Plus MMC Diff:{corr_plus_mmc_sharpe_diff}"
+    )
+
+    # Check correlation with example predictions
+    corr_with_example_preds = np.corrcoef(validation_example_preds.rank(pct=True, method="first"),
+                                          validation_data[PREDICTION_NAME].rank(pct=True, method="first"))[0, 1]
+    print(f"Corr with example preds: {corr_with_example_preds}")
+
     # Save predictions as a CSV and upload to https://numer.ai
     tournament_data[PREDICTION_NAME].to_csv(TOURNAMENT_NAME + "_submission.csv")
+
+
+""" 
+functions used for advanced metrics
+"""
+
+
+# to neutralize a column in a df by many other columns
+def neutralize(df, columns, by, proportion=1.0):
+    scores = df.loc[:, columns]
+    exposures = df[by].values
+
+    # constant column to make sure the series is completely neutral to exposures
+    exposures = np.hstack(
+        (exposures,
+         np.asarray(np.mean(scores)) * np.ones(len(exposures)).reshape(-1, 1)))
+
+    scores = scores - proportion * exposures.dot(
+        np.linalg.pinv(exposures).dot(scores))
+    return scores / scores.std()
+
+
+# to neutralize any series by any other series
+def neutralize_series(series, by, proportion=1.0):
+    scores = series.values.reshape(-1, 1)
+    exposures = by.values.reshape(-1, 1)
+
+    # this line makes series neutral to a constant column so that it's centered and for sure gets corr 0 with exposures
+    exposures = np.hstack(
+        (exposures,
+         np.array([np.mean(series)] * len(exposures)).reshape(-1, 1)))
+
+    correction = proportion * (exposures.dot(
+        np.linalg.lstsq(exposures, scores, rcond=None)[0]))
+    corrected_scores = scores - correction
+    neutralized = pd.Series(corrected_scores.ravel(), index=series.index)
+    return neutralized
+
+
+def unif(df):
+    x = (df.rank(method="first") - 0.5) / len(df)
+    return pd.Series(x, index=df.index)
+
+
+def get_feature_neutral_mean(df):
+    feature_cols = [c for c in df.columns if c.startswith("feature")]
+    df.loc[:, "neutral_sub"] = neutralize(df, [PREDICTION_NAME],
+                                          feature_cols)[PREDICTION_NAME]
+    scores = df.groupby("era").apply(
+        lambda x: correlation(x["neutral_sub"], x[TARGET_NAME])).mean()
+    return np.mean(scores)
 
 
 if __name__ == '__main__':
