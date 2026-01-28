6377bfdb62edb2566c0ad435b2b68cc37a4fd5ef
Michael Phillips <michael@numer.ai>
2026-01-28 11:28:54 -0800

    remove mlp regressor stuff

--- numerai/agents/code/modeling/utils/model_factory.py
+++ numerai/agents/code/modeling/utils/model_factory.py
@@ -9,22 +9,8 @@ def build_model(
 ):
     if model_type == "LGBMRegressor":
         from agents.code.modeling.models.lgbm_regressor import LGBMRegressor
-
         return LGBMRegressor(feature_cols=feature_cols, **model_params)
-    if model_type == "ResidualMLPRegressor":
-        from agents.code.modeling.models.residual_mlp_regressor import (
-            ResidualMLPRegressor,
-        )
-
-        return ResidualMLPRegressor(feature_cols=feature_cols, **model_params)
-    if model_type == "ResidualTorchMLPRegressor":
-        from agents.code.modeling.models.residual_torch_mlp_regressor import (
-            ResidualTorchMLPRegressor,
-        )
-
-        return ResidualTorchMLPRegressor(feature_cols=feature_cols, **model_params)
     raise ValueError(
         "Unsupported model type: "
-        f"{model_type}. Supported types: LGBMRegressor, ResidualMLPRegressor, "
-        "ResidualTorchMLPRegressor."
+        f"{model_type}. Supported types: LGBMRegressor"
     )
